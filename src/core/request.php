3212f394871a2870aa0f8a8ce4b115d4333b6108
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-15 09:03:11 -0300

    Adding request handle

---
+++ src/core/request.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace viking\core;
+
+class request{
+
+
+    public static function getUri()
+    {
+        return trim(
+            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
+        );
+    }
+
+    public static function getMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+}
