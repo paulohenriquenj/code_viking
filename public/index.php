5de4884f50814a02a625ef697618e1977c9917c5
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-15 22:24:53 -0300

    Using config

--- public/index.php
+++ public/index.php
@@ -4,4 +4,12 @@ require __DIR__ . '/../src/core/core.php';
 
 require __DIR__ . '/../src/core/router.php';
 
+require __DIR__ . '/../src/core/request.php';
 
+
+viking\core\router::load(
+    (viking\core\config::get('path'))['root'] . '/src/app/routes.php'
+)->redirect(
+    viking\core\request::getMethod(),
+    viking\core\request::getUri()
+);
