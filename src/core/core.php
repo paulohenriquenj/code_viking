f2ff465b58863f76e89dc9f80c4ee173273ed423
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-15 22:25:36 -0300

    Using config class

--- src/core/core.php
+++ src/core/core.php
@@ -1,4 +1,10 @@
 <?php
 
-require_once __DIR__.'/../../vendor/autoload.php';
+require 'config.php';
+
+viking\core\config::loadConfig(__DIR__ . '/../../config.php');
+
+
+require_once (viking\core\config::get('path'))['root'].'/vendor/autoload.php';
+
 
