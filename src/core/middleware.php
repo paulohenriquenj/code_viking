9f0418abc75372115579eeeb7dff6fb0b5590af9
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-16 21:45:26 -0300

    adding core middleware

---
+++ src/core/middleware.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace viking\core;
+
+use viking\core\middlewares\middleware as interfaceMiddleware;
+
+class middleware{
+
+    protected $middlewares;
+
+    protected $middlewaresChain;
+
+    public function __construct()
+    {
+        $this->loadDefaultMiddleware();
+    }
+
+    public function loadDefaultMiddleware()
+    {
+        if ($this->hasConfigMiddlewares()) {
+            foreach (config::middlewares() as $middlewareName) {
+                $middlewareClassName = 'viking\\core\\middlewares\\' . $middlewareName;
+                $middlewareClass = new $middlewareClassName;
+
+                if ($this->isMiddleware($middlewareClass)) {
+                    $this->middlewares[] = $middlewareClass;
+
+                }
+            }
+        }
+    }
+
+    protected function createChain()
+    {
+        // echo '<pre>';
+        // print_r($middlewaresReverse);
+        // echo '</pre>';
+        $middlewaresReverse = array_reverse($this->middlewares, true);
+        
+        foreach ($middlewaresReverse as $key => $middleware) {
+            $keyMinusOne = $key - 1;
+            
+            if ($keyMinusOne >= 0) {
+                $this->middlewares[$keyMinusOne]->setNext($middleware);
+            }
+        }
+
+        $this->middlewaresChain = $this->middlewares[0];
+    }
+
+    protected function hasConfigMiddlewares()
+    {
+        return !empty(config::middlewares());
+    }
+
+    protected function isMiddleware($class)
+    {
+        return $class instanceof interfaceMiddleware;
+    }
+
+    public function register(interfaceMiddleware $middleware)
+    {
+        $this->middlewares [] = $middleware;
+
+        return $this;
+    }
+
+    public function handle($request)
+    {
+        $this->createChain();
+
+        $this->middlewaresChain->handle($request);
+        
+    }
+    
+}
