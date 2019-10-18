c188e394d3a0df35cb16d7eef6b3cefcbfbe73f9
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-17 23:13:57 -0300

    Adding database connection info

--- config.php
+++ config.php
@@ -1,12 +1,26 @@
 <?php
 
 return [
+
+    // set some application paths
     'path' => [
         'root' => __DIR__
     ],
 
+    // set default middlewares
     'middlewares' => [
         'authMiddleware'
+    ],
+
+    // set database info
+    'database' => [
+        'name' => 'viking',
+        'username' => 'root',
+        'password' => '123456',
+        'connection' => 'mysql:host=172.18.0.2',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
     ]
 
 ];
