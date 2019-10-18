cc89f67c57ac449c1e9a8226fe073a7ee80d0fcc
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-17 22:59:52 -0300

    Adding router reference

--- src/core/middlewares/controllerMiddleware.php
+++ src/core/middlewares/controllerMiddleware.php
@@ -4,13 +4,17 @@ namespace viking\core\middlewares;
 
 use viking\core\middlewares\middleware;
 use viking\core\request;
+use viking\core\router;
 
 class controllerMiddleware implements middleware
 {
-
-    
-
     private $nextMiddleware;
+
+    public function __construct(router $router)
+    {
+        $this->router = $router;
+    }
+
 
     public function setNext(middleware $middleware)
     {
@@ -19,6 +23,11 @@ class controllerMiddleware implements middleware
 
     public function handle(request $request) 
     {
-        
+        return $this->router->callController(
+            ...explode(
+                '@', 
+                $this->router->routes[$request->method][$request->uri]
+            )
+        );
     }
 }
