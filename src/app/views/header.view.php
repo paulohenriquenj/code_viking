34c0de88e94afd79d855f95ec4686e41d5b1bd85
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-19 16:38:59 -0300

    Adding basic header

---
+++ src/app/views/header.view.php
@@ -0,0 +1,10 @@
+<?php
+
+return <<< html
+<div class="container mb-3">
+  <div class="row">
+    <div class="col top-left"></div>
+    <div class="col top-right"><a href="/login">Entrar</a></div>
+  </div>
+</div>
+html;
