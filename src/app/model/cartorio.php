9c60eb2bbca19c564a2d3a54f4acef8a693bf9f7
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-19 16:38:09 -0300

    Refactoring insert counter

--- src/app/model/cartorio.php
+++ src/app/model/cartorio.php
@@ -7,24 +7,44 @@ use viking\app\model\model;
 
 class cartorio extends model
 {
+    const REGISTER_INSERTED   = 1;
+    const REGISTER_DUPLICATED = 2;
+    const REGISTER_FAILED     = 3;
+
     public function insertCartorio($cartorioInfo)
     {
         $fieldsArray = xml2array($cartorioInfo);
 
         $fields = array_map(
             function ($item) {
-                return wrapperAndSlashes(utf8_decode($item));
+                return wrapperAndSlashes(
+                    utf8_decode($this->removeNULLstring(trim($item)))
+                );
             },
             $fieldsArray
         );
 
-        if (empty($this->fetch('cartorio', ['id'], 'nome = '.$fields['nome'].''))) {
-            return $this->insert('cartorio', $fields);
+        if (empty($this->fetch('cartorio', ['id'], 'documento = '.$fields['documento'].' AND nome = '.$fields['nome'].''))) {
+
+            if ($this->insert('cartorio', $fields)) {
+                return self::REGISTER_INSERTED;
+            }
+
+        } else {
+            return self::REGISTER_DUPLICATED;
         }
 
-        return false;
+        return self::REGISTER_FAILED;
 
     }
 
+    public function removeNULLstring($item)
+    {
+        if (strtolower($item) == 'null') {
+            return '';
+        }
+
+        return $item;
+    }
 
 }
