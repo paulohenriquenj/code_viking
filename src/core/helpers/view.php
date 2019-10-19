789fbabd61ea087d85ef8b295db166f692df7943
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-18 22:02:41 -0300

    Refactoring

--- src/core/helpers/view.php
+++ src/core/helpers/view.php
@@ -3,21 +3,37 @@
 
 function view($viewName, $data = [], $layout="html")
 {
-    $content = '';
-    $content_msg = '';
+    $content = [
+        'content' => '',
+        'header'  => '',
+        'aside'   => '',
+        'footer'  => '',
+        'msg'     => '',
+    ];
+
     extract($data);
 
     $layout = require viking\core\config::pathRoot() . '/src/app/views/layout/' . $layout . '.view.php';
 
     if (!empty($viewName)) {
-        $content = require viking\core\config::pathRoot() . '/src/app/views/' . $viewName . '.view.php';
+        foreach ((array) $viewName as $key => $viewItem) {
+
+            if (is_int($key)) {
+                $key ='content';
+            }
+
+            $content[$key] = require viking\core\config::pathRoot() . '/src/app/views/' . $viewItem . '.view.php';
+        }
     }
     
-    if (isset($msg)) {
-        $content_msg = require viking\core\config::pathRoot() . '/src/app/views/msg.view.php';
+    if (!empty($msg)) {
+        $content['msg'] = require viking\core\config::pathRoot() . '/src/app/views/msg.view.php';
     }
 
-    $layout = str_replace('__msg__', $content_msg, $layout);
-    echo str_replace('__main_content__', $content, $layout);
+    $layout = str_replace('__header__', $content['header'], $layout);
+    $layout = str_replace('__aside__', $content['aside'], $layout);
+    $layout = str_replace('__footer__', $content['footer'], $layout);
+    $layout = str_replace('__msg__', $content['msg'], $layout);
+    echo str_replace('__main_content__', $content['content'], $layout);
 
 }
