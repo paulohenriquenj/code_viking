9af835b8c79f5c5055bd3c978dbb3bac8976994b
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-19 10:45:44 -0300

    Fix link url

--- src/app/views/admin_aside.view.php
+++ src/app/views/admin_aside.view.php
@@ -3,7 +3,7 @@
 return <<< html
 <div class="list-group">
   <a href="/admin/import/xml" class="list-group-item list-group-item-action">Importar XML</a>
-  <a href="#" class="list-group-item list-group-item-action">Editar cartório</a>
+  <a href="/admin/edit/cartorio" class="list-group-item list-group-item-action">Editar cartório</a>
   <a href="#" class="list-group-item list-group-item-action">Exportar planilha Excel</a>
   <a href="#" class="list-group-item list-group-item-action">Enviar informativo</a>
 </div>
