78d5d1ecf65e34d9df8870c0fb059ed10bceccaa
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-18 21:41:02 -0300

    Adding partials html

---
+++ src/app/views/admin_aside.view.php
@@ -0,0 +1,10 @@
+<?php
+
+return <<< html
+<div class="list-group">
+  <a href="/admin/import/xml" class="list-group-item list-group-item-action">Importar XML</a>
+  <a href="#" class="list-group-item list-group-item-action">Editar cartório</a>
+  <a href="#" class="list-group-item list-group-item-action">Exportar planilha Excel</a>
+  <a href="#" class="list-group-item list-group-item-action">Enviar informativo</a>
+</div>
+html;
---
+++ src/app/views/admin_header.view.php
@@ -0,0 +1,10 @@
+<?php
+
+return <<< html
+<div class="container mb-3">
+  <div class="row">
+    <div class="col top-left"><img src="https://www.anoreg.org.br/site/wp-content/images/logo-anoreg-300.png" alt="Imagem logo anoreg" height="100"></div>
+    <div class="col top-right"><a href="/user/logout">sair</a></div>
+  </div>
+</div>
+html;
