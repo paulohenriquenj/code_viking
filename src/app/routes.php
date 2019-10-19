d2943761ca2ac7d28c6ef0274331aba59e0187fc
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-18 21:43:05 -0300

    Setting admim routes

--- src/app/routes.php
+++ src/app/routes.php
@@ -3,5 +3,12 @@
 
 $router->get('', 'home@index');
 $router->get('home', 'controller@home');
+
 $router->get('login', 'user@index');
 $router->post('login', 'user@login');
+
+$router->get('admin', 'admin@index');
+$router->get('admin/import/xml', 'admin@importXml');
+$router->post('admin/import/xmlfile', 'admin@importXmlFile');
+
+$router->get('user/logout', 'user@logout');
