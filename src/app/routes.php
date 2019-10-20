91f5f76b23d56efe7222f9185b3415d79f1d2857
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-20 15:09:52 -0300

    Adding router news

--- src/app/routes.php
+++ src/app/routes.php
@@ -8,14 +8,24 @@ $router->get('login', 'user@index');
 $router->post('login', 'user@login');
 
 $router->get('admin', 'admin@index');
-$router->get('admin/import/xml', 'admin@importXml');
-$router->post('admin/import/xml', 'admin@importXmlFile');
+
+$router->get('admin/import/xml', 'file@importXml');
+$router->post('admin/import/xml', 'file@importXmlFile');
+$router->get('admin/import/xls', 'file@importXls');
+$router->post('admin/import/xls', 'file@importXlsFile');
+
+
+$router->get('admin/list/cartorio', 'admin@list');
 $router->get('admin/edit/cartorio', 'admin@editCartorio');
 $router->post('admin/search/cartorio', 'admin@searchCartorio');
 $router->get('admin/edit/cartorio/info', 'admin@editCartorioInfo');
 $router->post('admin/edit/cartorio/info', 'admin@updataCartorioInfo');
 $router->get('admin/delete/cartorio', 'admin@deleteCartorio');
 $router->get('admin/export/cartorio', 'admin@exportCartorio');
+
+$router->get('admin/news/cartorio', 'news@news');
+$router->post('admin/edit/news', 'news@editNews');
+$router->get('admin/send/cartorio', 'news@newsSend');
 
 
 $router->get('user/logout', 'user@logout');
