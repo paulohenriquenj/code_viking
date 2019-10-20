8cba0ce99094d33bbd823e2356781fcf0be3a7f7
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-20 15:22:20 -0300

    Adding relation of fields names

--- src/app/model/cartorio.php
+++ src/app/model/cartorio.php
@@ -11,17 +11,27 @@ class cartorio extends model
     const REGISTER_DUPLICATED = 2;
     const REGISTER_FAILED     = 3;
 
-    public function insertCartorio($cartorioInfo)
-    {
-        $fieldsArray = xml2array($cartorioInfo);
+    public $fieldsNames=[];
 
-        $fields = array_map(
-            function ($item) {
-                return wrapperAndSlashes(
+    public function __construct() 
+    {
+        parent::__construct();
+        $this->setfieldsNamesRelation();
+    }
+
+    public function insertCartorio($cartorios)
+    {
+        // $fieldsArray = xml2array($cartorioInfo);
+
+        $fields = [];
+        array_walk(
+            $cartorios,
+            function ($item, $key) use (&$fields) {
+                $fieldName = $this->findFieldDBName($key);
+                $fields [$fieldName] = wrapperAndSlashes(
                     utf8_decode($this->removeNULLstring(trim($item)))
                 );
-            },
-            $fieldsArray
+            }
         );
 
         if (empty($this->fetch('cartorio', ['id'], 'documento = '.$fields['documento'].' AND nome = '.$fields['nome'].''))) {
@@ -45,6 +55,83 @@ class cartorio extends model
         }
 
         return $item;
+    }
+
+    public function setfieldsNamesRelation()
+    {
+        $this->fieldsNames ['database'] = $this->namesDatabase();
+        $this->fieldsNames ['xml'] = $this->namesXml();
+        $this->fieldsNames ['xls'] = $this->namesXls();
+    }
+
+    public function namesDatabase()
+    {
+        return [
+            'nome'           => 'nome',
+            'razao'          => 'razao',
+            'tipo_documento' => 'tipo_documento',
+            'documento'      => 'documento',
+            'cep'            => 'cep',
+            'endereco'       => 'endereco',
+            'bairro'         => 'bairro',
+            'cidade'         => 'cidade',
+            'uf'             => 'uf',
+            'tabeliao'       => 'tabeliao',
+            'ativo'          => 'ativo',
+            'telefone'       => 'telefone',
+            'email'          => 'email'
+        ];
+    }
+
+    public function namesXml()
+    {
+        return [
+            $this->fieldsNames['database']['nome']           => 'nome',
+            $this->fieldsNames['database']['razao']          => 'razao',
+            $this->fieldsNames['database']['tipo_documento'] => 'tipo_documento',
+            $this->fieldsNames['database']['documento']      => 'documento',
+            $this->fieldsNames['database']['cep']            => 'cep',
+            $this->fieldsNames['database']['endereco']       => 'endereco',
+            $this->fieldsNames['database']['bairro']         => 'bairro',
+            $this->fieldsNames['database']['cidade']         => 'cidade',
+            $this->fieldsNames['database']['uf']             => 'uf',
+            $this->fieldsNames['database']['tabeliao']       => 'tabeliao',
+            $this->fieldsNames['database']['ativo']          => 'ativo',
+            $this->fieldsNames['database']['telefone']       => 'telefone',
+            $this->fieldsNames['database']['email']          => 'email'
+        ];
+    }
+
+    public function namesXls()
+    {
+        return [
+            $this->fieldsNames['database']['nome']      => 'NOME',
+            $this->fieldsNames['database']['razao']     => 'RAZÃO',
+            $this->fieldsNames['database']['documento'] => 'DOCUMENTO',
+            $this->fieldsNames['database']['cep']       => 'CEP',
+            $this->fieldsNames['database']['endereco']  => 'ENDEREÇO',
+            $this->fieldsNames['database']['bairro']    => 'BAIRRO',
+            $this->fieldsNames['database']['cidade']    => 'CIDADE',
+            $this->fieldsNames['database']['uf']        => 'UF',
+            $this->fieldsNames['database']['telefone']  => 'TELEFONE',
+            $this->fieldsNames['database']['email']     => 'E-MAIL',
+            $this->fieldsNames['database']['tabeliao']  => 'TABELIÃO',
+            $this->fieldsNames['database']['ativo']     => 'ATIVO',
+        ];
+    }
+
+    public function findFieldDBName($fieldName)
+    {
+        $availableNames = array_keys($this->fieldsNames);
+
+        foreach ($availableNames as $nameType) {
+            foreach ($this->fieldsNames[$nameType] as $nameDB => $name) {
+                if ($fieldName == $name) {
+                    return $this->fieldsNames['database'][$nameDB];
+                }
+            }
+        }
+        return '';
     }
 
 }
