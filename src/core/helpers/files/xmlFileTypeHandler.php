053f81f53455611c0e9740769d99fd0f252d8189
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-10-20 15:23:06 -0300

    Fix xml parse

--- src/core/helpers/files/xmlFileTypeHandler.php
+++ src/core/helpers/files/xmlFileTypeHandler.php
@@ -8,9 +8,14 @@ class xmlFileTypeHandler implements fileTypeHandler
 {
     public function load(string $filePath)
     {
-        return (xml2array(
-            simplexml_load_file($_FILES['file']['tmp_name'])
-        ))['cartorio'];
+        return array_map(
+            function ($item) {
+                return xml2array($item);
+            },
+            (xml2array(
+                simplexml_load_file($_FILES['file']['tmp_name'])
+            ))['cartorio']
+        );
     }
 }
 
